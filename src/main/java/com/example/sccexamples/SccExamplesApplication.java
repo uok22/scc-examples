f3a95abfc0d0fb7bc5e3ad0e92937c8ac471507f
Unto Valkonen <unto.j.valkonen@icloud.com>
2020-03-03 12:12:29 +0200

    first commit

---
+++ src/main/java/com/example/sccexamples/SccExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sccexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccExamplesApplication.class, args);
+	}
+
+}
